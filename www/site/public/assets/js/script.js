f3287644e1f0c6eb42157c892a8f70250a7b9596
Deniss <denazebiz@gmail.com>
2019-09-01 13:28:21 +0100

    adding little bit of js

--- www/site/public/assets/js/script.js
+++ www/site/public/assets/js/script.js
@@ -1,1 +1,8 @@
-console.log(document.styleSheets)
+// const header =
+document.getElementById('header').innerHTML = `<div class="container">Header Container</div>`;
+
+// const content =
+document.getElementById('content').innerHTML = `<div class="container">Content container</div>`;
+
+// const footer =
+document.getElementById('footer').innerHTML = `<div class="container">Footer Container</div>`;
