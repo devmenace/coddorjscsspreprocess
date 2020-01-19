f257861058e0c44a87a2a242f5bd70e30df06edd
Den21 <menacedev@gmail.com>
2020-01-19 11:48:07 +0000

    small update

--- www/site/public/assets/js/script.js
+++ www/site/public/assets/js/script.js
@@ -1,11 +1,20 @@
+/*const data = require('dataFile.js');
+console.log(data);
+console.log(data.name);*/
+const readStuff = [];
+const learnStuff = [];
+const buyStuff = [];
+
+
 // const header =
 document.getElementById('header').innerHTML = `<div class="container">Header Container</div>`;
-
 // const content =
 document.getElementById('content').innerHTML = `<div class="container">Content container</div>`;
 
 // const footer =
 document.getElementById('footer').innerHTML = `<div class="container">Footer Container</div>`;
+
+
 // console.log(hostname);
 console.log('Number of styles on the page:  ' + document.styleSheets.length);
 console.log(document.styleSheets);
@@ -14,3 +23,4 @@ console.log('\n');
 console.log('Number of scripts on the page:  ' + document.scripts.length);
 console.log(document.scripts);
 console.log('\n');
+
